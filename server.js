b023bfb673ed157884484547d00bafd53403311d
Rushs321 <fyggh566@tutamail.com>
2024-08-30 16:11:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ const proxy = require('./src/proxy')
 const PORT = process.env.PORT || 8080
 
 app.enable('trust proxy')
-app.get('/', authenticate, params, proxy)
+app.get('/', proxy)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
 app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`))
